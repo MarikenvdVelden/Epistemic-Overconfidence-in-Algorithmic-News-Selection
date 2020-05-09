138cb83cefe15742805a7b06bc3f9a31f6b556b3
Mariken van der Velden <m.a.c.g.vander.velden@vu.nl>
2020-05-09 10:45:47 +0200

    US info

--- src/README.md
+++ src/README.md
@@ -3,4 +3,4 @@ This folder should contain the scripts that processes the data  before actual an
 # Scripts
 
 - [US Sample](data-processing/00_PrepareData_US.md) Cleans and constructs measures according to PAP (pp. 10--13, 18) for the US MTurk Sample.
-- [Dutch Sample](data-processing/00_PrepareData_US.md) Cleans and constructs measures according to PAP (pp. 13--18) for the Dutch PollFish Sample.
+- [Dutch Sample](data-processing/00_PrepareData_NL.md) Cleans and constructs measures according to PAP (pp. 13--18) for the Dutch PollFish Sample.
---
+++ src/lib/Functions.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(sjPlot)
+library(sjmisc)
+
+qualtrics <- function(df){
+  tmp <- read_csv(df, skip = 3, col_names = F)
+  colnames(tmp) <- read_csv(df, col_names = FALSE, skip = 0, n_max=1)
+  colnames(tmp)<-make.names(colnames(tmp), unique = TRUE) #duplicate column names in original file
+  return(tmp)
+}
+rename1 <- function(variable){
+  variable <- ifelse(variable == "never", 0,
+                     ifelse(variable == "every day", 7, variable))
+  variable <- as.numeric(variable)
+  return(variable)
+}
+rename2 <- function(variable){
+  variable <- ifelse(variable == "completely disagree", 1,
+                     ifelse(variable == "completely agree", 7, variable))
+  variable <- as.numeric(variable)
+  return(variable)
+}
+rename3 <- function(variable){
+  variable <- ifelse(variable == 2, 1,
+                     ifelse(variable == 3, 1, 0))
+  variable <- as.numeric(variable)
+  return(variable)
+}
